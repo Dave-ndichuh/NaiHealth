480f7c74265ee8b7ba4b1a9c9cfc9bfbc50b19a6
dave-ndichuh <machahdavis@gmail.com>
2024-11-13 10:42:43 +0300

    Add new files and update existing ones for Django project

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    fullname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    age = models.IntegerField()
+    yob = models.DateField()
+
+    def __str__(self):
+        return self.fullname
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class Patient(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1)
+    email = models.EmailField()
+    phonenumber= models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+
+    def __str__ (self):
+        return self.firstname + self.lastname
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    datetime = models.DateTimeField()
+    department = models.CharField(max_length=200)
+    doctor = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,11 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('services', views.service, name='services'),
-    path('starter', views.starter, name='starter'),
+    path('services/', views.service, name='services'),
+    path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('myservice/', views.myservice, name='myservice'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('contact/', views.contact, name='contact'),
+    path('appointment/', views.appointment, name='appointment'),
 ]
